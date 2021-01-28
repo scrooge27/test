ab1176d51548544a7a24fa9e91270d46999ffa63
Simone Pucci <simopucci2002@gmail.com>
2021-01-28 23:31:09 +0100

    es fatti

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const products = require("./routers/products")
+//const products = require("./routers/products")
 
 const port=4000;
 
@@ -26,9 +26,74 @@ app.get("/", (req,res)=>{
   .catch(err => console.error(err));
 });
 
+const prova=["cane","gatto","matita","bella","mare","ciao"];
+let counter=0;
+var list=[""];
+app.get("/accreditamento", (req,res)=>{
+	res.send(giasone);
+	console.log(count(prova));
+	console.log(words(prova));
+});
 
+app.post("/somma-pari", (req,res)=>{
+	num=req.body.chiavearray
+	res.send(sumEvens(num))
+});
 
-app.use("/products", products);
+app.put("/lista-parole-a",(req,res)=>{
+	counter=0;
+	words=req.body.chiavearray
+	parole = {
+	count : count(words),
+	words : words(words)
+	}
+	res.send(parole)
+
+})
+
+app.post("/comments",(req,res)=>{
+	//id=req.body.sth
+	fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+  .then(response => response.json())
+  .then(data =>{
+  	value=data[0].body.split(" ").length;
+  	res.send(commenti={
+  	count : value
+  })
+  				})	
+  .catch(err => console.error(err));
+})
+
+giasone = {
+	nome : "Simone",
+	cognome : "Pucci"
+}
+
+const sumEvens = arr => arr.filter(e => !(e % 2)).reduce((a, b) => a + b);
+const count = arr =>{
+	for (i = 0; i < arr.length; i++) {
+		if(arr[i].length===4){
+			counter++
+		}
+	}
+	return counter;
+}
+const words = arr =>{
+	counter=0;
+	for (i = 0; i < arr.length; i++) {
+		if(arr[i].length===4){
+			list[counter]=arr[i];
+			counter++
+		}
+	}
+	return list.join(" ");
+}
+
+function WordCount(str) { 
+  return str.split(" ").length;
+}
+
+//app.use("/products", products);
 
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
